deb71c7974c2313ea8ebf8c220a32ea17a3f4e31
vengeance <139268370+vyuq@users.noreply.github.com>
2023-07-18 16:09:44 -0400

    Create TOH-Loader.lua

---
+++ TOH-Loader.lua
@@ -0,0 +1,9 @@
+rconsolename("Galaxy Hub | Westbound")
+rconsoleprint("[1/3] Validating User \n")
+wait(1)
+rconsoleprint("[2/3] Loading Galaxy \n")
+wait(0.5)
+-- loadstring not yet
+rconsoleprint("[3/3] Loaded, enjoy galaxy! (Minimize this")
+wait(5) 
+rconsoleclear()
