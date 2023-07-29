f81540b2d80aefe678409efaf02d4c54ce808548
SkyGodCheesey <94723811+SkyGodCheesey@users.noreply.github.com>
2023-07-29 12:46:49 -0400

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,15 @@
+if game.GameId == 873703865 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/Westbound.lua", true))()
+elseif game.GameId == 65241 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/NDS.lua", true))()
+elseif game.GameId == 923621948 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/Texting-Simulator.lua", true))()
+elseif game.GameId == 703124385 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/TOH.lua", true))()
+elseif game.GameId == 4214708235 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/EverySecondPlusOne.lua", true))()
+elseif game.GameId == 598419145 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/OnlyUpBlox.lua", true))()
+else
+    game:GetService("Players").LocalPlayer:Kick("Game Not Supported")
+end
