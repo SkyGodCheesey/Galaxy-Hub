ebbec5db16ab4591b36b756fbde770f7e828db4e
Galaxy Developer <139268370+uhgalaxy@users.noreply.github.com>
2023-07-29 13:43:26 -0400

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -10,6 +10,8 @@ elseif game.GameId == 4214708235 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/EverySecondPlusOne.lua", true))()
 elseif game.GameId == 598419145 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/OnlyUpBlox.lua", true))()
+elseif game.GameId == 2619619496 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/Bedwars.lua", true))()
 else
     game:GetService("Players").LocalPlayer:Kick("Game Not Supported")
 end
