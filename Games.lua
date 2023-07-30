68abf7ceb87413de70854040e31a7225d8662f93
SkyGodCheesey <94723811+SkyGodCheesey@users.noreply.github.com>
2023-07-30 17:02:32 -0400

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -12,10 +12,12 @@ elseif game.GameId == 598419145 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/OnlyUpBlox.lua", true))()
 elseif game.GameId == 495693931 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/DoomspireBB.lua", true))()
-elseif game:GetService("Workspace").Lobby and game:GetService("Workspace").Lobby.Queues and game:GetService("Workspace").Lobby.Queues["bedwars_classic"] then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/Bedwars.lua", true))()
 elseif game.GameId == 210851291 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/BABFT.lua", true))()
+elseif game.PlaceId == 2248408710 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/DestructionSim.lua", true))()
+elseif game:GetService("Workspace").Lobby and game:GetService("Workspace").Lobby.Queues and game:GetService("Workspace").Lobby.Queues["bedwars_classic"] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyGodCheesey/Galaxy-Hub/main/Bedwars.lua", true))()
 else
     game:GetService("Players").LocalPlayer:Kick("Game Not Supported")
 end
